32260f46916c13d364fceea2438bb003ee707585
Deyvid Yury <deyvidyury@yahoo.com.br>
2017-07-06 01:11:50 -0300

    App ready

---
+++ image-search/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ImageListComponent } from './image-list/image-list.component';
+
+import { ImageService } from './shared/image.service';
+
+import { MaterialModule } from '@angular/material';
+import { MasonryModule } from 'angular2-masonry';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImageListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    MaterialModule,
+    MasonryModule,
+    BrowserAnimationsModule
+  ],
+  providers: [ImageService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ image-search/src/app/image-list/image-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../shared/image.service';
+
+@Component({
+  selector: 'app-image-list',
+  templateUrl: './image-list.component.html',
+  styleUrls: ['./image-list.component.css']
+})
+export class ImageListComponent implements OnInit {
+	images: any[];
+	imagesFound: boolean = false;
+	searching: boolean = false;
+
+  constructor(private _imageService: ImageService) { }
+
+  handleSuccess(data){
+  	this.images = data.hits;
+  	this.imagesFound = true;
+  }
+
+  handleError(error){
+  	console.log(error)
+  }
+
+  searchImages(query: string){
+  	this.searching = true;
+  	return this._imageService.getImage(query).subscribe(
+  		data => this.handleSuccess(data), 
+  		error => this.handleError(error), 
+  		() => this.searching = false
+  	)
+  }
+
+  ngOnInit() {
+  }
+
+}
